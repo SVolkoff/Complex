018ab16d2890d2c9ee9b65eb0c0cf969428fcff9
SVolkoff <sabina-volkoff@yandex.com>
2017-03-21 09:18:05 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -8,68 +8,68 @@ SCENARIO("default constructor") {
 }
 
 SCENARIO("constructor with params") {
-	complex_t complex(1, 2);
-	REQUIRE(complex.real_() == 1);
-	REQUIRE(complex.imaginary_() == 2);
+	complex_t complex(3, 4);
+	REQUIRE(complex.real_() == 3);
+	REQUIRE(complex.imaginary_() == 4);
 }
 
 SCENARIO("copy constructor") {
-	complex_t complex(1, 2);
+	complex_t complex(3, 4);
 	complex_t copy(complex);
-	REQUIRE(copy.real_() == 1);
-	REQUIRE(copy.imaginary_() == 2);
+	REQUIRE(copy.real_() == 3);
+	REQUIRE(copy.imaginary_() == 4);
 }
 
 SCENARIO("operator *") {
-	complex_t c1(7, 6);
-	complex_t c2(5, 4);
-	complex_t c3(11, 58);
+	complex_t c1(5, 4);
+	complex_t c2(3, 2);
+	complex_t c3(7, 22);
 	REQUIRE((c1*c2)==c3);
 }
 
 SCENARIO("operator /") {
-	complex_t c1(7, 6);
-	complex_t c2(7, 6);
-	complex_t c3(1, 0);
+	complex_t c1(5, 4);
+	complex_t c2(1, 2);
+	complex_t c3(2.6, -1.2);
 	REQUIRE((c1/c2)==c3);
 }
 
 SCENARIO("operator +=") {
-	complex_t c1(13, 1); 
-	complex_t c2(7, 6);
-	complex_t c3(20, 7);
+	complex_t c1(4, 5); 
+	complex_t c2(6, 7);
+	complex_t c3(10, 12);
 	REQUIRE((c1+=c2)==c3);
 }
 
 SCENARIO("operator -=") {
-	complex_t c1(13, 10); 
-	complex_t c2(7, 6);
-	complex_t c3(6, 4);
+	complex_t c1(15, 10); 
+	complex_t c2(5, -1);
+	complex_t c3(10, 11);
 	REQUIRE((c1-=c2)==c3);
 }
 
 SCENARIO("operator *=") {
-	complex_t c1(7, 6); 
-	complex_t c2(5, 4);
-	complex_t c3(11, 58);
+	complex_t c1(5, 4); 
+	complex_t c2(3, 2);
+	complex_t c3(7, 22);
 	REQUIRE((c1*=c2)==c3);
 }
 
 SCENARIO("operator /=") {
-	complex_t c1(7, 6); 
-	complex_t c2(7, 6);
-	complex_t c3(1, 0);
+	complex_t c1(5, 4);
+	complex_t c2(1, 2);
+	complex_t c3(2.6, -1.2);
 	REQUIRE((c1/=c2)==c3);
 }
 
 SCENARIO("operator =") {
-	complex_t c1(7, 6); 
+	complex_t c1(5, 6); 
 	complex_t c2=c1;
 	REQUIRE(c2==c1);
 }
 
 SCENARIO("operator ==") {
-	complex_t c1(7, 6); 
-	complex_t c2(7, 6);
+	complex_t c1(5, 6); 
+	complex_t c2(5, 6);
 	REQUIRE(c1==c2);
 }
