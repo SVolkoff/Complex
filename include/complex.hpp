c2dca1fe76f64ab876e27494741617877af201f6
SVolkoff <sabina-volkoff@yandex.com>
2017-03-21 09:10:13 +0300

    Update complex.hpp

--- include/complex.hpp
+++ include/complex.hpp
@@ -5,21 +5,21 @@ using namespace std;
 class complex_t
 {
 private:
-	float real;
-	float imaginary;
+	double  real;
+	double  imaginary;
 public:
 	complex_t();
-	complex_t(float rl, float img);
+	complex_t(double  rl, double  im);
 	complex_t(const complex_t&copy);
-	float real_();
-	float imaginary_();
+	double  real_();
+	double  imaginary_();
 	complex_t operator * (const complex_t& c2) const;
 	complex_t operator / (const complex_t& c2) const;
 	complex_t operator += (const complex_t& c2);
 	complex_t operator -= (const complex_t& c2);
 	complex_t operator *= (const complex_t& c2);
 	complex_t operator /= (const complex_t& c2);
-	complex_t operator = (const complex_t& result); 
+	complex_t operator = (const complex_t& result);
 	bool operator == (const complex_t& c2) const;
 	friend istream& operator >> (istream&cin, complex_t& result);
 	friend ostream& operator << (ostream&cout, complex_t& result);
