45bf970a526be1ed94ce536853c140cc39195016
SVolkoff <sabina-volkoff@yandex.com>
2017-03-21 09:10:44 +0300

    Update complex.cpp

--- sources/complex.cpp
+++ sources/complex.cpp
@@ -2,103 +2,103 @@
 
 complex_t::complex_t() 
 {
-	real=0;
-	imaginary=0;
+	real = 0;
+	imaginary = 0;
 }
 
-complex_t::complex_t(float rl, float img) 
+complex_t::complex_t(double  rl, double  im)
 {
-	real=rl;
-	imaginary=img;
+	real = rl;
+	imaginary = im;
 }
 
-complex_t::complex_t(const complex_t&copy)
+complex_t::complex_t(const complex_t&cpycmplx)
 {
-	real = copy.real;
-	imaginary = copy.imaginary;
-} 
+	real = cpycmplx.real;
+	imaginary = cpycmplx.imaginary;
+}
 
-float complex_t::real_()
+double complex_t::real_()
 {
 	return real;
 }
 
-float complex_t::imaginary_()
+double  complex_t::imaginary_()
 {
 	return imaginary;
 }
 
-bool complex_t::operator == (const complex_t& c2) const
+bool complex_t::operator == (const complex_t& compl2) const
 {
-	bool f=0;
-	if ((real == c2.real) && (imaginary == c2.imaginary))
-		f=1;
+	bool f = 0;
+	if ((real == compl2.real) && (imaginary == compl2.imaginary))
+		f = 1;
 	return f;
 }
 
-complex_t complex_t::operator = (const complex_t& result)
+complex_t complex_t::operator = (const complex_t& compl2)
 {
-	real = result.real;
-	imaginary = result.imaginary;
+	real = compl2.real;
+	imaginary = compl2.imaginary;
 	return *this;
 }
 
-istream& operator >> (istream&cin, complex_t& result)
+istream& operator >> (istream&cin, complex_t& compl)
 {
 	cout << "enter real part" << endl;
-	cin >> result.real;
+	cin >> compl.real;
 	cout << "enter imaginary part" << endl;
-	cin >> result.imaginary;
+	cin >> compl.imaginary;
 	cout << endl;
 	return cin;
 }
 
-ostream& operator << (ostream&cout, complex_t& result)
+ostream& operator << (ostream&cout, complex_t& compl)
 {
-	if (result.imaginary < 0)
+	if (compl.imaginary < 0)
 	{
-		cout << result.real << result.imaginary << "i" << endl;
+		cout << compl.real << compl.imaginary << "i" << endl;
 	}
 	else
-		cout << result.real << "+" << result.imaginary << "i" << endl;
+		cout << compl.real << "+" << compl.imaginary << "i" << endl;
 	return cout;
 }
 
-complex_t complex_t::operator * (const complex_t& c2) const
+complex_t complex_t::operator * (const complex_t& compl2) const
 {
-	return complex_t (real*c2.real-c2.imaginary*imaginary, c2.imaginary*real+c2.real*imaginary); 
+	return complex_t(real*compl2.real - compl2.imaginary*imaginary, compl2.imaginary*real + compl2.real*imaginary);
 }
 
-complex_t complex_t::operator / (const complex_t& c2) const
+complex_t complex_t::operator / (const complex_t& compl2) const
 {
-	return complex_t ((real*c2.real + imaginary*c2.imaginary) / (c2.real*c2.real + c2.imaginary*c2.imaginary), (c2.real*imaginary - real*c2.imaginary) / (c2.real*c2.real + c2.imaginary*c2.imaginary));
+	return complex_t((real*compl2.real + imaginary*compl2.imaginary) / (compl2.real*compl2.real + compl2.imaginary*compl2.imaginary), (compl2.real*imaginary - real*compl2.imaginary) / (compl2.real*compl2.real + compl2.imaginary*compl2.imaginary));
 }
 
-complex_t complex_t::operator += (const complex_t& c2)
+complex_t complex_t::operator += (const complex_t& compl2)
 {
-	real += c2.real;
-	imaginary += c2.imaginary;
+	real += compl2.real;
+	imaginary += compl2.imaginary;
 	return *this;
 }
 
-complex_t complex_t::operator -= (const complex_t& c2)
+complex_t complex_t::operator -= (const complex_t& compl2)
 {
-	real -= c2.real;
-	imaginary -= c2.imaginary;
+	real -= compl2.real;
+	imaginary -= compl2.imaginary;
 	return *this;
 }
-complex_t complex_t::operator /= (const complex_t& c2)
+complex_t complex_t::operator /= (const complex_t& compl2)
 {
-	float real_ = (real*c2.real + imaginary*c2.imaginary) / (c2.real*c2.real + c2.imaginary*c2.imaginary);
-	imaginary = (c2.real*imaginary - real*c2.imaginary) / (c2.real*c2.real + c2.imaginary*c2.imaginary);
-	real = real_;
+	double realtmp = (real*compl2.real + imaginary*compl2.imaginary) / (compl2.real*compl2.real + compl2.imaginary*compl2.imaginary);
+	imaginary = (compl2.real*imaginary - real*compl2.imaginary) / (compl2.real*compl2.real + compl2.imaginary*compl2.imaginary);
+	real = realtmp;
 	return *this;
 }
 
-complex_t complex_t::operator *= (const complex_t& c2)
+complex_t complex_t::operator *= (const complex_t& compl2)
 {
-	float real_ = real*c2.real - imaginary*c2.imaginary;
-	imaginary = real*c2.imaginary + c2.real*imaginary;   
-	real = real_;
+	double realtmp = real*compl2.real - imaginary*compl2.imaginary;
+	imaginary = real*compl2.imaginary + compl2.real*imaginary;
+	real = realtmp;
 	return *this;
 }
