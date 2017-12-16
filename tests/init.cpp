73248661d15ac02372f8ba2f0ffc9eb62caa0e5d
SVolkoff <sabina.volkoff@gmail.com>
2017-12-16 18:44:05 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -1,75 +1,36 @@
-#include <complex.hpp>
+#include <test.hpp>
 #include <catch.hpp>
 
-SCENARIO("default constructor") {
-	complex_t complex;
-	REQUIRE(complex.real_() == 0);
-	REQUIRE(complex.imaginary_() == 0);
+SCENARIO("array init") 
+{
+	array<int, 13> v1;
+	REQUIRE(v1.size() == 13);
+	REQUIRE(v1.empty() == false);
+	REQUIRE(v1[0]==0);
+	array<int, 0> v2;
+	REQUIRE(v1.size() == 0);
+	REQUIRE(v1.empty() == true);
+	array<int, 5> v3{1,5,7,23};
+	array<int, 5> v4(v3);
+	REQUIRE(v3[0]==v4[0]==1);
+	REQUIRE(v3[1]==v4[1]==5);
+	REQUIRE(v3[2]==v4[2]==7);
+	REQUIRE(v3[3]==v4[3]==23);
 }
 
-SCENARIO("constructor with params") {
-	complex_t complex(3, 4);
-	REQUIRE(complex.real_() == 3);
-	REQUIRE(complex.imaginary_() == 4);
-}
-
-SCENARIO("copy constructor") {
-	complex_t complex(3, 4);
-	complex_t copy(complex);
-	REQUIRE(copy.real_() == 3);
-	REQUIRE(copy.imaginary_() == 4);
-}
-
-SCENARIO("operator *") {
-	complex_t c1(5, 4);
-	complex_t c2(3, 2);
-	complex_t c3(7, 22);
-	REQUIRE((c1*c2)==c3);
-}
-
-SCENARIO("operator /") {
-	complex_t c1(5, 4);
-	complex_t c2(1, 2);
-	complex_t c3(2.6, -1.2);
-	REQUIRE((c1/c2)==c3);
-}
-
-SCENARIO("operator +=") {
-	complex_t c1(4, 5); 
-	complex_t c2(6, 7);
-	complex_t c3(10, 12);
-	REQUIRE((c1+=c2)==c3);
-}
-
-SCENARIO("operator -=") {
-	complex_t c1(15, 10); 
-	complex_t c2(5, -1);
-	complex_t c3(10, 11);
-	REQUIRE((c1-=c2)==c3);
-}
-
-SCENARIO("operator *=") {
-	complex_t c1(5, 4); 
-	complex_t c2(3, 2);
-	complex_t c3(7, 22);
-	REQUIRE((c1*=c2)==c3);
-}
-
-SCENARIO("operator /=") {
-	complex_t c1(5, 4);
-	complex_t c2(1, 2);
-	complex_t c3(2.6, -1.2);
-	REQUIRE((c1/=c2)==c3);
-}
-
-SCENARIO("operator =") {
-	complex_t c1(5, 6); 
-	complex_t c2=c1;
-	REQUIRE(c2==c1);
-}
-
-SCENARIO("operator ==") {
-	complex_t c1(5, 6); 
-	complex_t c2(5, 6);
-	REQUIRE(c1==c2);
+SCENARIO("array at, back, front, data, operator[]")
+{
+	array<int, 2> v1{9,12};
+	int tmp = v1.at(0);
+	REQUIRE(tmp == 9);
+	REQUIRE(tmp == v1.front());
+	REQUIRE(tmp == *v1.data());
+	REQUIRE(tmp == *v1.begin());
+	tmp = v1.at(1);
+	REQUIRE(tmp == 12);
+	REQUIRE(tmp == v1.back());
+	REQUIRE(tmp == *(v1.data() + 1));
+	REQUIRE(tmp == *v1.end());
+	REQUIRE(v1[0] == 9);
+	REQUIRE(v1[1] == 12);
 }
